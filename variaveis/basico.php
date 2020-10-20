66aa5f9ed8cec47525ededd9fd0e31f5d6b7830c
Leonardo Puppo <leonardopuppo@outlook.com>
2020-10-20 00:12:24 -0300

    Update assets

---
+++ variaveis/basico.php
@@ -0,0 +1,38 @@
+<div class="titulo">Variáveis</div>
+
+<?php
+$numeroA = 13;
+echo $numeroA, '<br>';
+var_dump($numeroA);
+
+echo '<br>';
+$a = 3;
+$b = 16;
+$soma = $a + $b;
+echo $soma;
+
+echo '<br>';
+echo isset($soma);
+
+unset($soma);
+echo '<br>';
+var_dump($soma);
+
+$variavel = 10;
+echo '<br>' . $variavel;
+
+$variavel = "Agora sou uma string";
+echo '<br>' . $variavel;
+
+// Nomes de variável, seja sempre consistente
+$var = 'valida';
+$var2 = 'valida';
+$VAR3 = 'valida';
+$_var_4 = 'valida';
+$vâr5 = 'valida'; // não aconselhável
+// $6var = 'invalida';
+// $%var7 = 'invalida';
+// $var8% = 'invalida';
+
+echo '<br>';
+var_dump($_SERVER["HTTP_HOST"]);
---
+++ variaveis/valor_referencia.php
@@ -0,0 +1,1 @@
+<div class="titulo">Valor vs Referência</div>
