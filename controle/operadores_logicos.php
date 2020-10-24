797f9240cca29805a17abb77b29e55b2fa859bb0
Leonardo Puppo <leonardopuppo@outlook.com>
2020-10-24 00:51:08 -0300

    Add

--- controle/operadores_logicos.php
+++ controle/operadores_logicos.php
@@ -1,29 +1,50 @@
 <div class="titulo">Operadores Logicos</div>
 
 <?php
-echo "<p><strong>V ou F</strong></p><hr>";
+echo "<p class='divisao'>V ou F</p><hr>";
 var_dump(true);
 echo '<br>';
 var_dump(!true);
 
-echo "<p><strong>Tabela Verdade 'AND' (E)</strong></p><hr>";
+echo "<p class='divisao'>Tabela Verdade 'AND' (E)</p><hr>";
 var_dump(true && true); // verdadeiro
 var_dump(true && false); // falso
 var_dump(false && true); // falso
 var_dump(false && false); // falso
 
-echo "<p><strong>Tabela Verdade 'OR' (OU)</strong></p><hr>";
-var_dump(true)
+echo "<p class='divisao'>Tabela Verdade 'OR' (OU)</p><hr>";
+var_dump(true || true);
+var_dump(true || false);
+var_dump(false || true);
+var_dump(false || false);
 
+echo "<p class='divisao'>Tabela Verdade 'XOR' (OU Exclusivo)</p><hr>";
+var_dump(true xor true);
+var_dump(true xor false);
+var_dump(false xor true);
+var_dump(false xor false);
 
-?>
+// Pode ser assim também
+var_dump(true != true);
+var_dump(true != false);
+var_dump(false != true);
+var_dump(false != false);
 
-<style>
-    p {
-        margin-bottom: 0px;    
-    }
+echo "<p class='divisao'>Exemplo</p><hr>";
+$idade = 65;
+$sexo = 'M';
 
-    hr {
-        margin-top: 0px;
-    }
-</style>
+$pagouPrevidencia = true;
+$criterioHomem = ($idade >= 65 && $sexo === 'M');
+$criterioMulher = ($idade >= 60 && $sexo === 'F');
+$atingiuCriterio = $criterioHomem || $criterioMuler;
+$podeSeAposentar = $pagouPrevidencia && $atingiuCriterio;
+echo "Pode se aposentar -> $podeSeAposentar.<br>";
+
+if($idade >= 60 && $sexo === 'F') {
+    echo "Pode se aposentar -> $sexo";
+} elseif ($idade >= 65 && $sexo === 'M') {
+    echo "Pode se aposentar -> $sexo";
+} else {
+    echo 'Vai ter que trabalhar mais um pouco...';
+}
