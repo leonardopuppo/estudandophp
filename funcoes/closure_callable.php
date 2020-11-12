8415e0ce1c54ab3dadf022dd5815c64601e2a81b
Leonardo Puppo <leonardopuppo@outlook.com>
2020-11-12 00:42:30 -0300

    Add aulas

---
+++ funcoes/closure_callable.php
@@ -0,0 +1,16 @@
+<div class="titulo">Closure & Callable</div>
+
+<?php
+$soma1 = function ($a, $b) {
+    return $a + $b;
+};
+
+function soma2($a, $b) {
+    return $a + $b;
+}
+
+echo $soma1(1, 3) . ' ';
+echo (is_callable($soma1) ? 'Sim' : 'Não') . '<br>';
+
+echo soma2(2, 3) . ' ';
+echo (is_callable(soma2) ? 'Sim' : 'Não') . '<br>';
--- index.php
+++ index.php
@@ -45,6 +45,21 @@
                                 Argumento Padrão
                             </a>
                         </li>
+                        <li>
+                            <a href="exercicio.php?dir=funcoes&file=usando_tipos">
+                                Usando Tipos
+                            </a>
+                        </li>
+                        <li>
+                            <a href="exercicio.php?dir=funcoes&file=anonimas">
+                                Funções Anônimas
+                            </a>
+                        </li>
+                        <li>
+                            <a href="exercicio.php?dir=funcoes&file=closure_callable">
+                                Closure & Callable
+                            </a>
+                        </li>
                     </ul>
                 </div>
                 <div class="modulo verde-escuro">
